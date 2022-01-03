3e43bf6947e0aaf949c35b44d62702a02a12d6e7
Cibin Joseph <34376424+cibinjoseph@users.noreply.github.com>
2022-01-04 00:00:04 +0530

    Update deprecated commands of selenium
    Replace deprecated function find_element_by_id() of selenium with find_element(). This prevents the deprecation warning from being printed too.

--- automation.py
+++ automation.py
@@ -11,6 +11,8 @@ pwd = 'password'
 import selenium
 import selenium.webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
 
 chrome_options = Options()
 chrome_options.add_argument("--headless") #Remove this to see the process
@@ -18,21 +20,21 @@ driver = selenium.webdriver.Chrome(options=chrome_options)
 
 driver.get('https://netaccess.iitm.ac.in/account/login')
 
-rollNo_box = driver.find_element_by_id('username')
+rollNo_box = driver.find_element(By.ID, 'username')
 rollNo_box.send_keys(rollNo)
 
-pwd_box = driver.find_element_by_id('password')
+pwd_box = driver.find_element(By.ID, 'password')
 pwd_box.send_keys(pwd)
 
-login_button = driver.find_element_by_id('submit')
+login_button = driver.find_element(By.ID, 'submit')
 login_button.click()
 
 driver.get('https://netaccess.iitm.ac.in/account/approve')
 
-oneDay = driver.find_element_by_id('radios-1')
+oneDay = driver.find_element(By.ID, 'radios-1')
 oneDay.click()
 
-approve_button = driver.find_element_by_id('approveBtn')
+approve_button = driver.find_element(By.ID, 'approveBtn')
 approve_button.click()
 
 #print("Successfully authenticated")
